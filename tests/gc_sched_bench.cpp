653b95525b53c76f9d152aab8b97c41277633411
Pavel <pbityukov76@gmail.com>
2025-03-27 22:46:59 +0000

    added bench case

--- tests/gc_sched_bench.cpp
+++ tests/gc_sched_bench.cpp
@@ -9,4 +9,4 @@ static void BM_GcSimulateActionsAutomaticCollect(benchmark::State& state) {
     gc_disable_auto();
     PerformMemoryActions<false>(state, state.range(0), state.range(1), state.range(2));
 }
-BENCHMARK(BM_GcSimulateActionsAutomaticCollect)->Args({1000, 1, 10})->Args({10000, 64, 512})->Args({1000, 1024, 4096})->UseRealTime()->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_GcSimulateActionsAutomaticCollect)->Args({1000, 1, 10})->Args({10000, 64, 512})->Args({1000, 1024, 4096})->Args({10000, 64, 1024})->UseRealTime()->Unit(benchmark::kMicrosecond);
